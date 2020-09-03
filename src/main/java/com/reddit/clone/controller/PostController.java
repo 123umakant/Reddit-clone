ad51c6ebd66818e821dc8aa713b22a5221052268
Ridham_Bodra <rhythmbodra@gmail.com>
2020-09-03 15:42:25 +0530

    added show posts

--- src/main/java/com/reddit/clone/controller/PostController.java
+++ src/main/java/com/reddit/clone/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.reddit.clone.controller;
 
+import com.reddit.clone.configurations.metadata.AwsS3Credentials;
 import com.reddit.clone.dto.TextPostDto;
 import com.reddit.clone.model.Post;
 import com.reddit.clone.service.FileService;
@@ -17,14 +18,18 @@ public class PostController {
 
     private PostService postService;
     private FileService fileService;
+    private AwsS3Credentials awsS3Credentials;
 
-    public PostController(PostService postService, FileService fileService) {
+    public PostController(PostService postService, FileService fileService, AwsS3Credentials awsS3Credentials) {
         this.postService = postService;
         this.fileService = fileService;
+        this.awsS3Credentials = awsS3Credentials;
     }
 
     @GetMapping("/show")
     public String showPosts(Model model) {
+
+        model.addAttribute("endpoint", awsS3Credentials.S3_BUCKET_NAME + "." + awsS3Credentials.S3_END_POINT);
 
         model.addAttribute("posts", postService.findAll());
 
@@ -45,8 +50,16 @@ public class PostController {
     @PostMapping("/create")
     public String post(@ModelAttribute("post") TextPostDto textPostDto, Model model, @RequestParam(value = "file", required = false) MultipartFile multipartFile) throws IOException {
 
-        if(textPostDto.getContentType().equals("media")){
+        if (textPostDto.getContentType().equals("media")) {
             String fileName = fileService.upLoadFile(multipartFile);
+
+            String extension = multipartFile.getOriginalFilename().substring(multipartFile.getOriginalFilename().lastIndexOf(".") + 1);
+
+            if (extension.equals("mp4")) {
+                textPostDto.setContentType("video");
+            } else {
+                textPostDto.setContentType("image");
+            }
 
             textPostDto.setContent(fileName);
         }
